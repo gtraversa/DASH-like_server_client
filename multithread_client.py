7ba6a176bf7a95feec753924f80aa957c17c922f
gtraversa <gtraversa@users.noreply.github.com>
2020-12-27 11:08:01 +0100

    added functionality

--- multithread_client.py
+++ multithread_client.py
@@ -11,14 +11,9 @@ email gianlu.traversa@gmail.com
 
 import socket,time,sys,getopt
 
-ClientSocket = socket.socket()
-host = '127.0.0.1'
-port = 1233
-
-
-
-
-
+_clientSocket = socket.socket()
+_host = '127.0.0.1'
+_port = 1233
 
 _prevBuf=0
 _tLast=0
@@ -26,20 +21,23 @@ _frameSize = 1024
 _start = time.time()                                                            
 _timer = 1                                                              #Minimum time in seconds between each request
 _maxBuf = 15                                                            #Maximum desired buffer in seconds (to avoid  unnecessary server load)
-_qualiReq = '_240p'
 _segNum=1
 
 
-def save_data_point(_point,_prevBuf,_tLast,_log):
+def save_data_point(_response,_prevBuf,_tLast,_log):
     """Function to process buffer health and log it to a file for analysis"""
+    _flgFinishDownload = 0
     _now = time.time()
-    _chunkInfo= str(_point).split(',')
+    _chunkInfo= str(_response).split(',')
     _point = _chunkInfo[0]
+    if str(_point) == '0':
+        print('Media finished.')
+        _flgFinish = 1
     _chunkSize=_chunkInfo[1]
     _tDiff = _now-_start-_tLast                                         #Time decrease as media plays
     _t,_buf = _now-_start,(float(_point)+_prevBuf-_tDiff)               #Time and buffer health new data points
     if _buf > 0:
-        _log.write(str(_t)+','+str(_buf)+'\n')                   #Heathy bffer
+        _log.write(str(_t)+','+str(_buf)+'\n')                          #Heathy bffer
     else:                                                               #Buffer event for negative buffer health
         _buf = 0
         _log.write(str(_t)+','+str(_buf)+'\n') 
@@ -47,7 +45,7 @@ def save_data_point(_point,_prevBuf,_tLast,_log):
         _req = 0
     else:
         _req=1 
-    return _buf,_t,_req,_chunkSize
+    return _buf,_t,_req,_chunkSize,_flgFinishDownload
 
 
 def create_setup(_bandwidth,_timer,_req):
@@ -55,18 +53,19 @@ def create_setup(_bandwidth,_timer,_req):
     _qualiReq = quali_select()
     _segNum = 1 #implement segment tracking
     _setup = str(_bandwidth)+','+ str(_req)+','+str(_timer) +','+str(_qualiReq)+','+str(_segNum)
-    return _setup
+    return _setup,_qualiReq
 
 def initial_setup(opts):
     """Parse initial setup arguments """
-    _bandwidth,_timer,_req = 100,1,1
+    _bandwidth,_timer,_req,_maxBuf = 100,1,1,10
     for o, a in opts:
         if o in ('-h','--help'):
            print("You're on your own")
            print('-h,--help')
-           print('-b,--bandwidth: Set the client maximum bandwidth')
+           print('-b,--bandwidth: Set the client maximum bandwidth (default:100Mbit)')
            print('-r,--request: Use if you desire client NOT to request data')
-           print('-t,--timer: Set client minimum time between requests')
+           print('-t,--timer: Set client minimum time between requests(default:1s)')
+           print('-m,--max_buffer: Set the maximum buffer desired for the client(default:10s)')
            sys.exit()
         elif o in ('-b','--bandwidth'):
             _bandwidth = int(a)
@@ -74,13 +73,15 @@ def initial_setup(opts):
             _timer = float(a)
         elif o in ('-r','--request'):
             _req = 0
+        elif o in ('-m','--maxbuffer'):
+            _maxBuf = int(a)
     connect()
-    _bufferData=create_log(_bandwidth,_timer)
+    _bufferData=create_log(_bandwidth,_timer,_maxBuf)
     return _bandwidth,_timer,_req,_bufferData
 
-def create_log(_bandwidth,_timer):
+def create_log(_bandwidth,_timer,_maxBuf):
     """Creates .txt file for logging buffer health with parameters in the name"""
-    _logName = str(_bandwidth)+'_'+str(_timer)+'_'+'_buffer.txt'
+    _logName = str(_bandwidth)+'_'+str(_timer)+'_'+str(_maxBuf)+'_buffer.txt'
     _bufferData = open(_logName,'w+')                                   #Logging file created for buffer health
     _bufferData.truncate(0)
     return _bufferData
@@ -91,32 +92,54 @@ def quali_select():
 
 def connect():
     print('Waiting for connection')
-    try:
-        ClientSocket.connect((host, port))
-    except socket.error as e:
-        print(str(e))
+    while True:
+        try:
+            _clientSocket.connect((_host, _port))
+            break
+        except socket.error as e:
+            print(str(e))
+
+def send_request(_clientSocket, _setup):
+    """Send media request to server"""
+    _clientSocket.send(str.encode(_setup))
+    
+
+def debug_prints(_response,_qualiReq):
+    """Print received data for debugging purposes"""
+    _chunkInfo= str(_response.decode('utf-8')).split(',')
+    _length = str(_chunkInfo[0])
+    _size = str(_chunkInfo[1])
+    _repr = str(_qualiReq)
+    print('_________________________')
+    print('Chunk length= '+_length) 
+    print('Chunk size= '+_size)
+    print('Chunk quality= '+_repr)                   
+    print('PrevBuf= '+ str(_prevBuf))
+
+def track_media():
+    """Tracks the current media being streamed and requests the next available chunk"""
+    pass
 
 def main(argv):
     global _prevBuf,_tLast
+    _flgFinishDownload = 0
     try:
-        opts, args = getopt.getopt(argv,"hb:t:r",["help","bandwidth=","timer=","request"])
+        opts, args = getopt.getopt(argv,"hb:t:rm:",["help","bandwidth=","timer=","request","max_buffer="])
     except getopt.GetoptError:
         print('Wrong inputs retardo')
         sys.exit(2)
     _bandwidth,_timer,_req,_bufferData = initial_setup(opts)
     while True:
-        _setup = create_setup(_bandwidth,_timer,_req)            #Pass setup to server,will be implemented with command line args for simplicity
-        ClientSocket.send(str.encode(_setup))
-        _response = ClientSocket.recv(_frameSize)
-        if _response.decode('utf-8')=='End':                                #End of media stream condition
-            break
-        _prevBuf,_tLast,_req,_chunkSize = save_data_point(_response.decode('utf-8'),_prevBuf,_tLast,_bufferData)
-        print('_________________________')
-        print('Response = '+_response.decode('utf-8'))                      #Print response from server for debugging
-        print('PrevBuf= '+ str(_prevBuf))
-        print('ChunkSize= '+ str(_chunkSize))
+        _setup,_qualiReq = create_setup(_bandwidth,_timer,_req)                       #Pass setup to server
+        send_request(_clientSocket,_setup)
+        _response = _clientSocket.recv(_frameSize)
+        if _flgFinishDownload:                                #End of media stream condition (not implemented yet on server side)
+            _prevBuf,_tLast,_req,_chunkSize,_flgFinishDownload = save_data_point('0,0',_prevBuf,_tLast,_bufferData)
+        else:
+            _prevBuf,_tLast,_req,_chunkSize,_flgFinishDownload = save_data_point(_response.decode('utf-8'),_prevBuf,_tLast,_bufferData)
+        debug_prints(_response,_qualiReq)
 
-    ClientSocket.close()
+    _clientSocket.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
