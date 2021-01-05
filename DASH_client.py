eb4599742aa3f68f373170e150dd2ad6d521538a
gtraversa <gtraversa@users.noreply.github.com>
2021-01-05 18:50:23 +0100

    naive quali select v1

--- DASH_client.py
+++ DASH_client.py
@@ -14,9 +14,11 @@ email gianlu.traversa@gmail.com
 
 import socket,time,sys,getopt
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 
 class Client:
-    def __init__(self,bandwidth=100,timer=1,req=1,max_buf=10,host_IP = '127.0.0.1', host_port = 1233, frame_size = 1024, connected = False, quali_req = '_240p'):
+    def __init__(self,bandwidth=100,timer=0.3,req=1,max_buf=10,host_IP = '127.0.0.1', host_port = 1233, frame_size = 1024, connected = False, quali_req = '_240p'):
         self.socket = socket.socket()
         self.host_IP = host_IP
         self.host_port = host_port
@@ -32,7 +34,15 @@ class Client:
         self.flg_finish_download = 0
         self.connected = connected
         self.quali_req = quali_req
+        self.stream_data = [[0,0,0,bandwidth]]
+        fig, (ax1,ax2,ax3) = plt.subplots(3)
+        self.fig = fig 
+        self.ax1 = ax1
+        self.ax2 = ax1
+        self.ax3 = ax1
         self.log_name = Client.create_log(self)
+        self.reprs = ['_240p','_480p','_720p','_1080p']
+        self.current_repr = quali_req
     
     def toString(self):
         """Print Attributes"""
@@ -49,7 +59,6 @@ class Client:
         log_name = str(self.bandwidth)+'_'+str(self.timer)+'_'+str(self.max_buf)+'_buffer.txt'
         self.buffer_data = open(log_name,'w+')                                   
         self.buffer_data.truncate(0)
-        self.buffer_data.write('0,0,0\n')
         return log_name
     
     def save_data_point(self,response):
@@ -66,10 +75,12 @@ class Client:
         t,buf = now-self.start,(float(point)+self.prev_buf-t_diff)               #Time and buffer health new data points
 
         if buf > 0:
-            self.buffer_data.write(str(t)+','+str(buf)+','+str(chunk_size)+'\n')                          #Heathy bffer
-        else:                                                               #Buffer event for negative buffer health
+            #self.buffer_data.write(str(t)+','+str(buf)+','+str(chunk_size)+'\n')                          #Heathy buffer
+            self.stream_data.append([t,buf,int(chunk_size)])
+        else:                                                                                             #Buffer event for negative buffer health
             buf = 0
-            self.buffer_data.write(str(t)+','+str(buf)+','+str(chunk_size)+'\n')
+            #self.buffer_data.write(str(t)+','+str(buf)+','+str(chunk_size)+'\n')
+            self.stream_data.append([t,buf,int(chunk_size)])
         if buf > self.max_buf:                                                  #Stop requesing media when buffer is saturated
             self.req = 0
         elif int(chunk_size) != -1:
@@ -78,7 +89,15 @@ class Client:
 
     def quali_select(self):
         """Basically the whole project: to implement"""
-        self.quali_req= '_240p'
+        current_repr_idx = self.reprs.index(self.current_repr)
+        if self.stream_data[-1][2]/self.stream_data[-1][3] > 1.1:
+            if current_repr_idx != len(self.reprs):
+                self.quali_req = self.reprs[current_repr_idx+1]               
+        elif self.stream_data[-1][2]/self.stream_data[-1][3] < 0.9:
+            if current_repr_idx != 0:
+                self.quali_req = self.reprs[current_repr_idx-1]     
+
+
 
     def connect(self):
         """Connect to media server"""
@@ -115,33 +134,35 @@ class Client:
         message = str(self.bandwidth)+','+ str(self.req)+','+str(self.timer) +','+str(self.quali_req)+','+str(self.seg_num)
         self.socket.send(str.encode(message))
         self.track_media()
+    
+    def save_file(self):
+        for point in self.stream_data:
+            for i in point:
+                self.buffer_data.write(str(i)+',')
+            self.buffer_data.write('\n')
 
-    def print_data_graph(self):
-        """Prints buffer and chunk data graph updated"""
-        fig = plt.figure()
-        ax1 = fig.add_subplot(1,1,1)
-        ax2 = ax1.twinx() 
-        graph_data = open(self.log_name,'r').read()
-        lines = graph_data.split('\n')
-        ts = []
-        bufs = []
-        chunks = []
-        for line in lines:
-            if len(line) > 1:
-                t, buf,chunk = line.split(',')
-                ts.append(float(t))
-                bufs.append(float(buf))
-                chunks.append(int(chunk))
+    def calc_bandwidth(self):
+        """Approximates bandwidth from chunk size and download time"""
+        if self.stream_data[-1][2]!= 0:
+            download_time = self.stream_data[-1][0]-self.stream_data[-2][0]
+            estimated_bandwidth = self.stream_data[-1][2]/download_time
+            self.stream_data[-1].append(estimated_bandwidth)
+        elif self.req==1:
+            for i in range(1,len(self.stream_data)):
+                if self.stream_data[-i][2] !=0:
+                    download_time = self.stream_data[-i][0]-self.stream_data[-i-1][0]
+                    estimated_bandwidth = self.stream_data[-i][2]/download_time
+                    self.stream_data[-1].append(estimated_bandwidth)
+                    break
+        elif self.stream_data[-1][2] == -1:
+            print('pepe')
+            self.stream_data[-1].append(-1)
+        else:
+            self.stream_data[-1].append(self.stream_data[-2][3])
 
-        ax1.plot(ts,bufs,color = 'tab:red')
-        ax1.set_xlabel('Simulation time (s)')
-        ax1.set_ylabel('Buffer health (s)')
-        ax1.tick_params(axis='y', labelcolor= 'tab:red')
-        ax2.plot(ts,chunks,color = 'tab:blue')
-        ax2.set_ylabel('Chunk sizes (MBit)')
-        ax2.tick_params(axis='y', labelcolor= 'tab:blue')
-        fig.tight_layout()
-        plt.show()
+    def avg_chunk(self):
+        """Calculate average chunk size"""
+        pass
 
     def start_request(self):
         """Begin media requests"""
@@ -149,7 +170,6 @@ class Client:
             self.connect()
             time.sleep(1)
         while True:
-            self.quali_select()
             self.send_request()
             self.response = self.socket.recv(self.frame_size)
             self.debug_prints()
@@ -160,6 +180,9 @@ class Client:
             if self.flg_finish_download==1 and self.prev_buf==0:
                 print('Disconnecting from server...')
                 break
+            self.calc_bandwidth()
+            self.quali_select()
+            print(self.stream_data[-1])
             #self.print_data_graph()
         self.socket.close()
 
@@ -167,3 +190,4 @@ class Client:
 
 c = Client()
 c.start_request()
+c.save_file()
