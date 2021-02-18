9908833395f0d78498c2c492a4a0423e87b44a04
gtraversa <gtraversa@users.noreply.github.com>
2021-02-18 21:53:32 +0000

    Array action fix

--- Current/Stepped/stable_testing.py
+++ Current/Stepped/stable_testing.py
@@ -4,21 +4,21 @@ from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 
 env = client.Client()
-# check_env(env)
+#check_env(env)
 
-# # Instantiate the agent
+# Instantiate the agent
 # model = DQN('MlpPolicy', env, learning_rate=1e-3)
 # # Train the agent
-# model.learn(total_timesteps=int(1e5))
+# model.learn(total_timesteps=int(1e4))
 # # Save the agent
-# model.save("test_1_1e-3_1e5")
+# model.save("test_1_1e-3_1e4")
 
 # mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
-
+# print(mean_reward)
 model = DQN.load("test_1_1e-3_1e5")
 # Enjoy trained agent
 obs = env.reset()
 for i in range(100):
-    action, _states = model.predict(obs)
+    action, _states = model.predict(obs, deterministic = True)
     obs, rewards, dones, info = env.step(action)
     
